c655c76cbf66cd1cffbc9dfa999afe87957a3c9d
dssgabriel <dss.gabriel@protonmail.com>
2022-06-16 16:00:08 +0200

    Fixed changed function name for GAXPY driver

--- include/drivers.h
+++ include/drivers.h
@@ -6,6 +6,6 @@ int driver_init(config_t *config);
 int driver_copy(config_t *config);
 int driver_reduc(config_t *config);
 int driver_dotprod(config_t *config);
-int driver_daxpy(config_t *config);
+int driver_gaxpy(config_t *config);
 int driver_vec_sum(config_t *config);
 int driver_vec_scale(config_t *config);
